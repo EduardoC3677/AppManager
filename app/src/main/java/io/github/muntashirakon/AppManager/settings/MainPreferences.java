dcc71ab64dd3a86870a920d1f8f10aacd95828ce
thejaustin <the.j.austin@gmail.com>
2025-12-16 02:33:51 -0600

    debug: Use minimal preferences_main_test.xml to isolate settings freeze issue

--- app/src/main/java/io/github/muntashirakon/AppManager/settings/MainPreferences.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/settings/MainPreferences.java
@@ -47,7 +47,7 @@ public class MainPreferences extends PreferenceFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.preferences_main, rootKey);
+        setPreferencesFromResource(R.xml.preferences_main_test, rootKey);
         getPreferenceManager().setPreferenceDataStore(new SettingsDataStore());
         MainPreferencesViewModel model = new ViewModelProvider(requireActivity()).get(MainPreferencesViewModel.class);
         mActivity = requireActivity();
